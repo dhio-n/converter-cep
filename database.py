537a9e3acfbf9e5c736878b5302727a26b8251f0
dhio-n <142285791+dhio-n@users.noreply.github.com>
2025-04-23 17:21:53 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from datetime import datetime
 import os
-
-
 
 def conectar():
     try:
+        password = os.getenv("SENHA")
+        if not password:
+            raise ValueError("A variável de ambiente 'SENHA' não foi definida.")
+
         conn = psycopg2.connect(
             user="postgres.qsqajbcsbuezstvnaofj",
-            password=os.environ["SENHA"],
+            password=password,
             host="aws-0-sa-east-1.pooler.supabase.com",
             port="6543",
             dbname="postgres",
