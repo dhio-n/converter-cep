f968d6857a086698235a32f1fd9b48bb5d8b4cac
dhio-n <142285791+dhio-n@users.noreply.github.com>
2025-04-23 16:21:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -49,11 +49,7 @@ def process_ceps(file):
 
     st.markdown("### 🔍 Processando CEPs:")
 
-   ''' for cep in ceps_formatados:
-        endereco_completo = buscar_endereco_brasil_cep(cep)
-        enderecos[cep] = endereco_completo
-        st.markdown(f"✅ **{cep}** → `{endereco_completo}`")
-        '''
+   
 
     for cep in ceps_formatados:
         endereco_completo = enderecos[cep]
